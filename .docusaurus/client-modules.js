00a7994c8b45fa2b684b0fdc0bbf4464fc62b489
Someshwar Patil <dedsecsp@gmail.com>
2026-02-06 14:18:52 +0530

    Manual deploy from AI agent

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/someshwarpatil/Code/Riverr/riverr-graphql-openapi/documentation/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/someshwarpatil/Code/Riverr/riverr-graphql-openapi/documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/someshwarpatil/Code/Riverr/riverr-graphql-openapi/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/someshwarpatil/Code/Riverr/riverr-graphql-openapi/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/someshwarpatil/Code/Riverr/riverr-graphql-openapi/documentation/src/css/custom.css"),
+];
